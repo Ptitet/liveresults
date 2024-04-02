dbc37272ac09a695877f7d7786e9cca03c2ac3d2
le.michel <ptitet73@gmail.com>
2024-04-02 17:27:30 +0200

    competitions appears in html

--- js/index.js
+++ js/index.js
@@ -1,7 +1,37 @@
 import LiveresultsAPI from './api.js';
 
 let competitions = await LiveresultsAPI.getCompetitions();
-console.log(competitions.length)
-let competition = await LiveresultsAPI.getCompetition(29070);
-console.log(competition);
-console.log(await LiveresultsAPI.getClasses(29070));
+
+function areDatesSameDay(date1, date2) {
+    return date1.getFullYear() === date2.getFullYear() &&
+        date1.getMonth() === date2.getMonth() &&
+        date1.getDate() === date2.getDate();
+}
+
+let todayDate = new Date();
+
+let notLiveTodayCompetitions = [];
+let liveTodayCompetitions = [];
+
+for (let competition of competitions) {
+    if (areDatesSameDay(new Date(competition.date), todayDate)) {
+        liveTodayCompetitions.push(competition);
+    } else {
+        notLiveTodayCompetitions.push(competition);
+    }
+}
+
+let liveTodayList = document.querySelector('section#live-today ul');
+let notLiveTodayList = document.querySelector('section#not-live-today ul');
+
+for (let competition of liveTodayCompetitions) {
+    let listItem = document.createElement('li');
+    listItem.textContent = competition.name;
+    liveTodayList.appendChild(listItem);
+}
+
+for (let competition of notLiveTodayCompetitions) {
+    let listItem = document.createElement('li');
+    listItem.textContent = competition.name;
+    notLiveTodayList.appendChild(listItem);
+}
